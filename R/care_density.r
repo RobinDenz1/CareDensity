3a2f0858070398897afe94feb9d868d920ddb5f2
Robin Denz <robindenz@web.de>
2024-05-28 13:38:15 +0200

    feat: add fragmented_care_density() function

--- R/care_density.r
+++ R/care_density.r
@@ -32,7 +32,29 @@ get_d_combs <- function(data, dat_g) {
   return(d_combs)
 }
 
-## calculate the classic care density as defined by Pollack
+## given a data.frame of weigts per connections, expand it to include
+## all equal connections (direction does not matter)
+expand_connection_weights <- function(weights) {
+  
+  setDT(weights)
+  
+  # get connection types
+  weights[, con := paste0(from, " - ", to)]
+  weights[, con_rev := paste0(to, " - ", from)]
+  weights[, from := NULL]
+  weights[, to := NULL]
+  
+  # put in long format and remove duplicates
+  weights <- melt(weights, id.vars="weight")
+  weights[, variable := NULL]
+  weights <- unique(weights)
+  colnames(weights) <- c("con_weight", "connection")
+  
+  return(weights)
+}
+
+## general function to calculate either the care density or the fragmented
+## care density
 #' @importFrom data.table setDT
 #' @importFrom data.table setcolorder
 #' @importFrom data.table setkey
@@ -45,17 +67,15 @@ get_d_combs <- function(data, dat_g) {
 #' @importFrom igraph V<-
 #' @importFrom igraph graph.adjacency
 #' @importFrom igraph get.data.frame
-#' @export
-care_density <- function(data, pat_col=1, as_data_frame=TRUE) {
+care_density.fit <- function(data, pat_col=1, weights, type,
+                             by_connection=FALSE, as_data_frame=TRUE,
+                             estimator) {
   
   requireNamespace("data.table")
   requireNamespace("igraph")
   
   # silence devtools::check()
   . <- PatID <- ArztID <- n <- weight <- sum_weights <- NULL
-  
-  check_inputs_care_density(data=data, pat_col=pat_col,
-                            as_data_frame=as_data_frame)
   
   # as data.table to make it faster
   setDT(data)
@@ -85,12 +105,13 @@ care_density <- function(data, pat_col=1, as_data_frame=TRUE) {
   }
   
   # create bipartite graph from data
-  g <- graph.data.frame(data, directed=FALSE)
-  V(g)$type <- bipartite.mapping(g)$type
+  g <- graph_from_data_frame(data, directed=FALSE)
+  V(g)$type <- bipartite_mapping(g)$type
   
   # project to provider mode
-  g_prov <- graph.adjacency(adjmatrix=project_to_one_mode(g, mode="cols"),
-                            mode="upper", weighted=TRUE, diag=FALSE)
+  mat <- project_to_one_mode(g, mode="cols")
+  g_prov <- graph_from_adjacency_matrix(adjmatrix=mat, mode="upper",
+                                        weighted=TRUE, diag=FALSE)
   
   # remove patients with just one connection
   d_counts <- data[, .(n = .N), by=PatID]
@@ -98,23 +119,83 @@ care_density <- function(data, pat_col=1, as_data_frame=TRUE) {
   data <- data[PatID %chin% d_counts[n > 1]$PatID]
   
   # extract weighted edge list
-  dat_g <- get.data.frame(g_prov)
+  dat_g <- as_data_frame(g_prov)
   
-  # weights of all occured connections
+  # weights of all occurred connections
   d_combs <- get_d_combs(data, dat_g)
   
-  # put together
-  out <- d_combs[, .(sum_weights = sum(weight)), by=PatID]
-  out <- merge(out, d_counts, by="PatID", all.x=TRUE)
-  
-  # apply formula
-  out[, care_density := sum_weights / (n * (n - 1) / 2)]
+  if (estimator=="simple") {
+    # put together
+    out <- d_combs[, .(sum_weights = sum(weight)), by=PatID]
+    out <- merge(out, d_counts, by="PatID", all.x=TRUE)
+    
+    # apply formula
+    out[, care_density := sum_weights / (n * (n - 1) / 2)]
+    
+  } else if (estimator=="fragmented") {
+    
+    # merge types to it
+    colnames(type) <- c("from", "type_from")
+    d_combs <- merge(d_combs, type, by="from", all.x=TRUE)
+    
+    colnames(type) <- c("to", "type_to")
+    d_combs <- merge(d_combs, type, by="to", all.x=TRUE)
+    
+    # create connection type ID
+    d_combs[, connection := paste0(type_from, " - ", type_to)]
+    d_combs[, type_from := NULL]
+    d_combs[, type_to := NULL]
+    
+    # calculate sums per connection type
+    out <- d_combs[, .(sum_weights = sum(weight)), by=list(PatID, connection)]
+    
+    # merge with n_p data
+    out <- merge(out, d_counts, by="PatID", all.x=TRUE)
+    
+    # calculate care density per connection type
+    out[, care_density := sum_weights / (n * (n - 1) / 2)]
+    
+    # calculate final fragmented care density
+    if (!by_connection) {
+      
+      # expand to get redundant connections as well
+      d_weights <- expand_connection_weights(weights)
+      
+      # check if some are missing
+      missing_con <- out$connection[!out$connection %in% d_weights$connection]
+      missing_con <- unique(missing_con)
+      
+      if (length(missing_con) > 0) {
+        stop("The following connection types are not defined in the 'weights'",
+             " argument: ", paste0(missing_con, collapse=", "), ".\n Please",
+             " add those to the 'weight' data.frame and rerun this function.")
+      }
+      
+      # merge with connection weights
+      out <- merge(out, d_weights, by="connection", all.x=TRUE)
+      
+      # calculate FCD
+      out <- out[, .(fragmented_care_density = sum(care_density*con_weight)),
+                 by=PatID]
+    }
+  }
   
   # add patients with just one contact back
   data0[, ArztID := NULL]
-  data0[, sum_weights := NA]
-  data0[, n := 1]
-  data0[, care_density := NA]
+  
+  if (estimator=="simple") {
+    data0[, sum_weights := NA]
+    data0[, n := 1]
+    data0[, care_density := NA]
+  } else if (by_connection) {
+    data0[, connection := NA_character_]
+    data0[, sum_weights := NA]
+    data0[, n := 1]
+    data0[, care_density := NA]
+  } else {
+    data0[, fragmented_care_density := NA]
+  }
+  
   out <- rbind(out, data0)
   setkey(out, "PatID")
   
@@ -122,5 +203,34 @@ care_density <- function(data, pat_col=1, as_data_frame=TRUE) {
     out <- as.data.frame(out)
   }
   
+  return(out)
+}
+
+## calculate the classic care density as defined by Pollack et al. (2013)
+#' @export
+care_density <- function(data, pat_col=1, as_data_frame=TRUE) {
+  
+  check_inputs_care_density(data=data, pat_col=pat_col,
+                            as_data_frame=as_data_frame)
+  
+  out <- care_density.fit(data=data, pat_col=pat_col,
+                          as_data_frame=as_data_frame,
+                          by_connection=FALSE, weights=NULL, type=NULL,
+                          estimator="simple")
+  return(out)
+}
+
+## calculate the fragmented care density as defined by Engels et al. (2024)
+#' @export
+fragmented_care_density <- function(data, pat_col=1, weights, type,
+                                    by_connection=FALSE, as_data_frame=TRUE) {
+  
+  check_inputs_fcd(data=data, pat_col=pat_col, weights=weights, type=type,
+                   by_connection=by_connection, as_data_frame=as_data_frame)
+  
+  out <- care_density.fit(data=data, pat_col=pat_col,
+                          as_data_frame=as_data_frame,
+                          by_connection=by_connection, weights=weights,
+                          type=type, estimator="fragmented")
   return(out)
 }
--- R/input_checks.r
+++ R/input_checks.r
@@ -15,3 +15,38 @@ check_inputs_care_density <- function(data, pat_col, as_data_frame) {
     stop("'as_data_frame' must be either TRUE or FALSE.")
   }
 }
+
+## input checks for fragmented_care_density()
+#' @importFrom data.table %chin%
+#' @importFrom data.table fifelse
+check_inputs_fcd <- function(data, pat_col, as_data_frame, type,
+                             weights, by_connection) {
+  
+  check_inputs_care_density(data=data, pat_col=pat_col,
+                            as_data_frame=as_data_frame)
+  
+  prov_col <- fifelse(pat_col==1, 2, 1)
+  data <- as.data.frame(data)
+  
+  if (!(length(by_connection)==1 && is.logical(by_connection))) {
+    stop("'by_connection' must be either TRUE or FALSE.")
+  } else if (!(is.data.frame(type) && ncol(type)==2 &&
+               all(colnames(type)==c("ID", "Type"))) &&
+               is.character(type$ID) && is.character(type$Type)) {
+    stop("'type' must be a data.frame with only two columns called",
+         " 'ID' and 'Type', both including only characters.")
+  } else if (!all(data[,prov_col][[1]] %chin% type$ID)) {
+    missing_prov <- data[,prov_col][[1]][!data[,prov_col][[1]] %chin% type$ID]
+    stop("The 'type' data.frame must contain information on all available",
+         " providers. Information not included for: ", missing_prov)
+  } else if (!by_connection && is.null(weights)) {
+    stop("'weights' must be a data.frame containing connection-specific",
+         " weights if by_connection=FALSE.")
+  } else if (!(by_connection || (is.data.frame(weights) && ncol(weights)==3 &&
+               all(colnames(weights)==c("from", "to", "weight")) &&
+               is.character(weights$from) && is.character(weights$to) &&
+               is.numeric(weights$weight)))) {
+    stop("'weights' must be a data.frame with exactly three columns called",
+         " 'from' (character), 'to' (character) and 'weight' (numeric).")
+  }
+}
